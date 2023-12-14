15cd2e485a56c1783a69d6300d8f7dfd634a0abd
cakepower <32028127+cakepower@users.noreply.github.com>
2023-12-14 10:12:05 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 basic.forever(function () {
     pins.servoWritePin(AnalogPin.P14, pins.map(
-    pins.analogReadPin(AnalogPin.P3),
-    0,
-    1023,
+    input.rotation(Rotation.Pitch),
+    -90,
+    90,
     -180,
     180
     ))
     pins.servoWritePin(AnalogPin.P15, pins.map(
-    pins.analogReadPin(AnalogPin.P3),
-    0,
-    1023,
+    input.rotation(Rotation.Pitch),
+    -90,
+    90,
     -180,
     180
     ))
